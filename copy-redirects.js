38e196397f80fa83dbc83b559de2f8526f586038
AKASH S <akash.settukannu@claysys.net>
2025-08-28 10:19:18 +0530

    fix

--- copy-redirects.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const src = path.join(__dirname, 'src', '_redirects');
-const dest = path.join(__dirname, 'dist', 'taxi-booking-app', 'browser', '_redirects');
-
-fs.copyFileSync(src, dest);
-console.log('_redirects copied successfully!');
